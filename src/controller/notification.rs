f0dce45fde948015fcc1ae3aebfc0492edfb240b
William Alexander <williamax2005@gmail.com>
2025-03-28 04:48:54 +0700

    Implement unsubscribe function in Notification controller

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -1,3 +1,5 @@
+use std::ops::Sub;
+
 use rocket::serde::json::Json;
 
 use bambangshop_receiver::Result;
@@ -8,6 +10,14 @@ use crate::service::notification::NotificationService;
 #[get("/subscribe/<product_type>")]
 pub fn subscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
     return match NotificationService::subscribe(product_type) {
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
+    };
+}
+
+#[get("/unsubscribe/<product_type>")]
+pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
+    return match NotificationService::unsubscribe(product_type) {
         Ok(f) => Ok(Json::from(f)),
         Err(e) => Err(e)
     };
